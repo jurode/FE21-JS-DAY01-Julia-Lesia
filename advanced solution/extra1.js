15a0f1696415421b873bfef077bb99974d5a9028
Julia Demel <julia.demel@gmail.com>
2021-03-03 13:33:22 +0100

    addition to extra.js

--- advanced solution/extra1.js
+++ advanced solution/extra1.js
@@ -1,14 +1,15 @@
-// EXTRA 1
+// == EXTRA 1
 
-// 1) From the following text:
-// "Hey there, i am a javascript developer, and i live in vienna"
+// * From the following text:
+// * "Hey there, i am a javascript developer, and i live in vienna"
 
-// the output to be this:
-// "I am a javascript developer."
+// * the output to be this:
+// * "I am a javascript developer."
 
 
-// our text before
+// == our text before
 var text = "Hey there, i am a javascript developer, and i live in vienna";
 
 // now put away some stuff
 console.log("I" + text.slice(12, 38) + ".");
+
--- advanced solution/extra2.js
+++ advanced solution/extra2.js
@@ -1,12 +1,22 @@
-// EXTRA 2
+// * EXTRA 2
 
-// 2) Create a program using different methods, which will provide the following outputs:
-// Start = ['apple', 'banana', 'kiwi']
+// *Create a program using different methods, which will provide the following outputs:
+// * Start = ['apple', 'banana', 'kiwi']
 
-// Output = ['apple', 'banana','orange','strawberry','kiwi'] 
-// Note: try to find a solution with less lines of code as you can.
+// * Output = ['apple', 'banana','orange','strawberry','kiwi'] 
+// * Note: try to find a solution with less lines of code as you can.
 
+// == Text before
 var start = ["apple", "banana", "kiwi"];
 
+
+// == Text after
+
+// # Version 1
+var start = ["apple", "banana", "kiwi"];
 start.splice(2,0,"orange", "strawberry");
 console.log(start);
+
+// # Version 2
+var otherVar = ["apple", "banana", "kiwi"].splice(2,0,"orange", "strawberry");
+console.log(otherVar); // ! this version is not working
