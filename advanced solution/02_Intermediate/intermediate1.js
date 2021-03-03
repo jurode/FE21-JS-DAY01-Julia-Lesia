6099815753422186bba601ab9861cf9074249473
Julia Demel <julia.demel@gmail.com>
2021-03-03 13:56:48 +0100

    add structure in advanced solution + index-file

--- advanced solution/02_Intermediate/intermediate1.js
+++ advanced solution/02_Intermediate/intermediate1.js
@@ -1,4 +1,13 @@
 // == INTERMEDIATE 1
 
+// Write a JavaScript program that will sort the following values alphabetically: Tesla, Audi, Renault, Volvo, Mazda, Fiat and Ferrari.
+
+// (Use an array to contain these values)
+
+
+
+// create array
 var autos = ['Tesla', 'Audi', 'Renault', 'Volvo', 'Mazda', 'Fiat', 'Ferrari'];
+
+// sort array
 document.getElementById("box2").innerHTML = autos.sort();
