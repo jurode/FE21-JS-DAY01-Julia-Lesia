31869f8c3b290404a270a9798e959ee8d30a6b25
Julia Demel <julia.demel@gmail.com>
2021-03-03 11:15:03 +0100

    add challenge, create index file

---
+++ basic4.js
@@ -0,0 +1,9 @@
+
+var players = ['Martin', 'Thomas', 'Peter', 'Mathias', 'Niki'];
+
+
+document.getElementById("box").innerHTML = "The most valuable player of the match is " + players[2];
+
+
+
+
---
+++ intermediate1.js
@@ -0,0 +1,2 @@
+var autos = ['Tesla', 'Audi', 'Renault', 'Volvo', 'Mazda', 'Fiat', 'Ferrari'];
+document.getElementById("box2").innerHTML = autos.sort();
